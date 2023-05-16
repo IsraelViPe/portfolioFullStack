5fd3b2790e2de2d6dd76148a9604f5d97e7d41ea
IsraelPereira <pereira.israel2070@gmail.com>
2023-05-16 09:38:41 -0300

    feat: estiliza foto da home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,12 @@
+import images from '../../assets/images'
 import './Home.scss'
 
 export default function Home() {
   return (
-    <div id="Home" className="container head-text">
-      Home
-    </div>
+    <section id="Home" className="cont__home head-text">
+      <div>
+        <img src={images.isViPe} alt="Foto Israel" />
+      </div>
+    </section>
   )
 }
--- src/assets/images.jsx
+++ src/assets/images.jsx
@@ -1,7 +1,11 @@
 import logo from './logo.svg'
+import isViPe from './isViPe.png'
+import IsViPe from './IsViPe.png'
 
 const images = {
-  logo
+  logo,
+  isViPe,
+  IsViPe
 }
 
 export default images
