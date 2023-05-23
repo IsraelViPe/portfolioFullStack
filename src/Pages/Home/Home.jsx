f7d69edb21d84cd6e89ed57b22545cbdacbce721
IsraelPereira <pereira.israel2070@gmail.com>
2023-05-23 08:25:14 -0300

    feat: adicionando efeitos de nav bar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,21 +1,72 @@
-import images from '../../assets/images'
-import { motion } from 'framer-motion'
+import { motion, useMotionValueEvent, useScroll } from 'framer-motion'
+import { useState } from 'react'
 import './Home.scss'
 
 export default function Home() {
+  const [showEmo, setShowEmo] = useState(false)
+  const { scrollY } = useScroll()
+
+  const heroText = ['oi, eu sou o', '</', 'isreal', '>']
+
+  useMotionValueEvent(scrollY, 'change', (x) => {
+    if (x > 50) setShowEmo(true)
+  })
   return (
     <section id="Home" className="cont__home">
-      <div className="cont_home-profile-img">
-        <img src={images.isViPe} alt="Foto Israel" />
+      <div>
+        {heroText.map((text) => {
+          const fullText = (
+            <motion.div
+              style={
+                (text === '</' || text === '>') && {
+                  color: 'var(--emphasis-color)'
+                }
+              }
+              whileInView={{ y: [-250, 20], opacity: [0, 0.5, 0.8, 1] }}
+              transition={{
+                duration: 0.8,
+                delay: text === '</' || text === '>' || text === '/' ? 0.8 : 0.1
+              }}
+            >
+              <h1>{text}</h1>
+            </motion.div>
+          )
+          return fullText
+        })}
       </div>
-      <motion.div
-        whileInView={{ x: [100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.8 }}
-        className="cont__home-info"
-      >
-        <h1 className="head-text">Oi, eu sou Israel</h1>
-        <p className="p-text">Desenvolvedor Web Full Stack</p>
-      </motion.div>
+      <div>
+        <motion.div
+          className="secondary"
+          whileInView={{
+            x: [-250, 20],
+            opacity: [0, 0.5, 0.8, 1],
+            transition: {
+              duration: 0.8,
+              delay: 0.8
+            }
+          }}
+        >
+          <h4>
+            Desenvolvedor Web <br /> Full Stack <br />
+          </h4>
+        </motion.div>
+        {showEmo && (
+          <motion.div
+            whileInView={{
+              opacity: [0, 1],
+              rotate: [0, 90]
+            }}
+          >
+            <span
+              style={{
+                color: 'var(--emphasis-color)'
+              }}
+            >
+              {';)'}
+            </span>
+          </motion.div>
+        )}
+      </div>
     </section>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,46 +1,60 @@
 import images from '../../assets/images'
 import SocialMedia from '../SocialMedia/SocialMedia'
 import { CgMenuRound, CgCloseO } from 'react-icons/cg'
-import { motion } from 'framer-motion'
+import { motion, useMotionValueEvent, useScroll } from 'framer-motion'
 import './NavBar.scss'
 import { useState } from 'react'
 
 export default function NavBar() {
   const [toggle, setToggle] = useState(false)
+  const [showNav, setShowNav] = useState(false)
+
+  const { scrollY } = useScroll()
+
+  useMotionValueEvent(scrollY, 'change', (x) => {
+    if (x > 20) setShowNav(true)
+    if (x < 20) setShowNav(false)
+  })
 
   return (
-    <nav className="cont__navbar">
-      <div className="cont__navbar-logo">
-        <img src={images.logo} alt="logo" />
-      </div>
-      <ul className="cont__nav-links">
-        {['Home', 'Sobre mim', 'Portfolio', 'Contato'].map((item) => (
-          <li key={item}>
-            <a href={`#${item}`}>{item}</a>
-          </li>
-        ))}
-      </ul>
+    <>
+      {showNav && (
+        <nav className="cont__navbar">
+          <div className="cont__navbar-logo">
+            <img src={images.logo} alt="logo" />
+          </div>
+          <ul className="cont__nav-links">
+            {['Home', 'Sobre mim', 'Portfolio', 'Contato'].map((item) => (
+              <li key={item}>
+                <a href={`#${item}`}>{item}</a>
+              </li>
+            ))}
+          </ul>
 
-      <div className="cont__navbar-hamburger">
-        <CgMenuRound onClick={() => setToggle(true)} />
+          <div className="cont__navbar-hamburger">
+            <CgMenuRound onClick={() => setToggle(true)} />
 
-        {toggle && (
-          <motion.div transition={{ duration: 0.85, ease: 'easeOut' }}>
-            <CgCloseO onClick={() => setToggle(false)} />
-            <ul>
-              {['Home', 'Sobre mim', 'Portfolio', 'Contacto'].map((item) => (
-                <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)}>
-                    {item}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </motion.div>
-        )}
-      </div>
+            {toggle && (
+              <motion.div transition={{ duration: 0.85, ease: 'easeOut' }}>
+                <CgCloseO onClick={() => setToggle(false)} />
+                <ul>
+                  {['Home', 'Sobre mim', 'Portfolio', 'Contacto'].map(
+                    (item) => (
+                      <li key={item}>
+                        <a href={`#${item}`} onClick={() => setToggle(false)}>
+                          {item}
+                        </a>
+                      </li>
+                    )
+                  )}
+                </ul>
+              </motion.div>
+            )}
+          </div>
 
-      <SocialMedia />
-    </nav>
+          <SocialMedia />
+        </nav>
+      )}
+    </>
   )
 }
