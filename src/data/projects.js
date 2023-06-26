434b8b0869faf212a204b203c6d4b95ba5cea46e
IsraelPereira <pereira.israel2070@gmail.com>
2023-06-26 20:56:35 -0300

    feat: adiciona um novo projeto

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,7 @@
 import recipesApp from '../assets/recipesApp.png'
 import storeManager from '../assets/storeManager.png'
 import emConstrucao from '../assets/emConstrucao.png'
+import apiBlog from '../assets/apiBlogs.png'
 
 const projects = [
   {
@@ -22,6 +23,16 @@ const projects = [
     codeLink: 'https://github.com/IsraelViPe/store-manager',
     title: 'StoreManager',
     tags: ['full stack', 'API rest', 'nodeJS']
+  },
+  {
+    name: 'API de Blogs',
+    img: apiBlog,
+    description:
+      'Uma API e um banco de dados para a produção de conteúdo para um blog!',
+    projectLink: '',
+    codeLink: 'https://github.com/IsraelViPe/blogs-api',
+    title: 'API de Blogs',
+    tags: ['API rest', 'nodeJS']
   },
   {
     name: 'Em breve',
